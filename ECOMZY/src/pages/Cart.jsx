8d00bfaa45ae6d67b896ef8ebb7ce01e7af41313
kishan04rajput <kishan9rajput@gmail.com>
2024-12-25 20:47:01 +0530

    ECOMZY-CSS-UPDATED

--- ECOMZY/src/pages/Cart.jsx
+++ ECOMZY/src/pages/Cart.jsx
@@ -12,10 +12,10 @@ const Cart = () => {
     }, [cart]);
 
     return (
-        <div className="flex justify-center p-4 ">
+        <div className="flex justify-center p-4 min-h-screen">
             {cart.length > 0 ? (
-                <div className="flex w-3/4 gap-4">
-                    <div className="w-2/3">
+                <div className="flex flex-col lg:flex-row w-full lg:w-3/4 gap-4">
+                    <div className="w-full lg:w-2/3">
                         {cart.map((item, index) => (
                             <CartItem
                                 key={item.id}
@@ -25,7 +25,7 @@ const Cart = () => {
                         ))}
                     </div>
 
-                    <div className="w-1/3 bg-gray-100 p-4 rounded-lg shadow-md">
+                    <div className="w-full lg:w-1/3 bg-gray-100 p-4 rounded-lg shadow-md">
                         <div className="flex justify-between items-center mb-4">
                             <div className="text-xl font-bold">Your Cart</div>
                             <div className="text-xl font-bold">Summary</div>
@@ -45,7 +45,7 @@ const Cart = () => {
                     </div>
                 </div>
             ) : (
-                <div className="flex flex-col justify-center items-center text-center w-full h-[70vh]">
+                <div className="flex flex-col justify-center items-center text-center w-full h-[80vh]">
                     <h1 className="text-2xl font-bold mb-4">Cart Empty</h1>
                     <Link to={"/"}>
                         <button className="bg-blue-500 text-white px-4 py-2 rounded">
--- ECOMZY/src/pages/Home.jsx
+++ ECOMZY/src/pages/Home.jsx
@@ -3,48 +3,46 @@ import Spinner from "../components/Spinner";
 import Product from "../components/Product";
 
 const Home = () => {
-  const API_URL = "https://fakestoreapi.com/products";
-  const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState([]);
+    const API_URL = "https://fakestoreapi.com/products";
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
 
-  async function fetchProductData() {
-    setLoading(true);
+    async function fetchProductData() {
+        setLoading(true);
 
-    try{
-      const res = await fetch(API_URL);
-      const data = await res.json();
+        try {
+            const res = await fetch(API_URL);
+            const data = await res.json();
 
-      setPosts(data);
+            setPosts(data);
+        } catch (error) {
+            console.log("Error aagya ji");
+            setPosts([]);
+        }
+        setLoading(false);
     }
-    catch(error) {
-      console.log("Error aagya ji");
-      setPosts([]);
-    }
-    setLoading(false);
-  }
 
-  useEffect( () => {
-    fetchProductData();
-  },[])
+    useEffect(() => {
+        fetchProductData();
+    }, []);
 
-  return (
-    <div>
-      {
-        loading ? <Spinner />  :
-        posts.length > 0 ? 
-        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
-          {
-            posts.map( (post) => (
-            <Product key = {post.id} post={post}/>
-          ) )
-          }
-        </div>) :
-        <div className="flex justify-center items-center">
-          <p>No Data Found</p>
-        </div> 
-      }
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-4">
+            {loading ? (
+                <Spinner />
+            ) : posts.length > 0 ? (
+                <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto gap-5 min-h-[80vh]">
+                    {posts.map((post) => (
+                        <Product key={post.id} post={post} />
+                    ))}
+                </div>
+            ) : (
+                <div className="flex justify-center items-center">
+                    <p>No Data Found</p>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Home;
