90856df30dfc234e1155914c28428b9696d0584a
kishan04rajput <kishan9rajput@gmail.com>
2026-07-24 02:24:34 +0530

    feat: reorder Education component below Projects section in App layout

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -11,10 +11,10 @@ function App() {
     <div className="bg-gray-100 min-h-screen">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-8 font-sans">
         <Header />
-        <Education />
         <WorkExperience />
         <Skills />
         <Projects />
+        <Education />
         <Additionals />
       </div>
     </div>
